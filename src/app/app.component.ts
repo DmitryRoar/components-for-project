7dc3b29ea7dca606d1842b70d85073394337c7c5
dmitryRoar <roarlove1337@gmail.com>
2021-03-23 19:01:47 +0700

    init before connect material angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  img = 'assets/img/user-photo.png';
+  lastLesson = '54:00';
+}
